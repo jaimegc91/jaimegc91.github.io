94c4ff23093bd9b9682a2492fa744c073fa41cd1
jaime garcia cazorla <jaime garcia cazorla>
2017-03-06 11:55:40 +0100

    Update curriculum

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
 (function () {
   'use strict';
 
-  
+
+  function descarga(archivo) {
+  document.location = archivo;
+  }  /* Aquí pondremos el código*/
 }());
-var dropdownMenu1 = document.getElementById('dropdownMenu1');
-  console.log('dropdownMenu1 = ', dropdownMenu1);
-
-var $elemento = $('#dropdownMenu1');
-
-
- $elemento.click(function(){
-   var classList = '';
-
-   if($elemento.hasClass('clicked')){
-     // Si myBtn contiene la clase active la elimino
-     $(this).removeClass('clicked');
-
-     // Recuperamos el atributo clase y los partimos
-     classList = $elemento.attr('class').split(' ');
-     console.log('classList === ', classList);
-   }else{
-     // En caso contrario la añado
-     $(this).addClass('clicked');
-
-         }
-           });
